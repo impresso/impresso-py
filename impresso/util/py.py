5be8a109795c842dd9916360320895d8e4ac3071
Roman Kalyakin <roman@kalyakin.com>
2024-04-03 21:51:28 +0200

    Refactor get_enum_from_literal function to handle Unset values in py.py

--- impresso/util/py.py
+++ impresso/util/py.py
@@ -1,11 +1,16 @@
 from enum import Enum
 from typing import TypeVar
 
+from impresso.api_client.types import Unset
+
 V = TypeVar("V", bound=str | int)
 E = TypeVar("E", bound=Enum)
 
 
-def get_enum_from_literal(value: V, enum: type[E]) -> E:
+def get_enum_from_literal(value: V, enum: type[E]) -> E | Unset:
+    if isinstance(value, Unset):
+        return value
+
     for member in enum:
         if member.value == value:
             return member
