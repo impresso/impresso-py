6169451e6f06a80300f37514a006d4d3341d9132
Roman Kalyakin <theorm@gmail.com>
2024-10-17 15:43:13 +0200

    regenerated schema (#8)

--- impresso/api_client/models/topic_related_topics_item.py
+++ impresso/api_client/models/topic_related_topics_item.py
@@ -1,6 +1,8 @@
-from typing import Any, Dict, Type, TypeVar
+from typing import Any, Dict, Type, TypeVar, Union
 
 from attrs import define as _attrs_define
+
+from ..types import UNSET, Unset
 
 T = TypeVar("T", bound="TopicRelatedTopicsItem")
 
@@ -11,15 +13,19 @@ class TopicRelatedTopicsItem:
     Attributes:
         uid (str): The unique identifier of the related topic
         w (float): TODO
+        avg (Union[Unset, float]): TODO
     """
 
     uid: str
     w: float
+    avg: Union[Unset, float] = UNSET
 
     def to_dict(self) -> Dict[str, Any]:
         uid = self.uid
 
         w = self.w
+
+        avg = self.avg
 
         field_dict: Dict[str, Any] = {}
         field_dict.update(
@@ -28,6 +34,8 @@ class TopicRelatedTopicsItem:
                 "w": w,
             }
         )
+        if avg is not UNSET:
+            field_dict["avg"] = avg
 
         return field_dict
 
@@ -38,9 +46,12 @@ class TopicRelatedTopicsItem:
 
         w = d.pop("w")
 
+        avg = d.pop("avg", UNSET)
+
         topic_related_topics_item = cls(
             uid=uid,
             w=w,
+            avg=avg,
         )
 
         return topic_related_topics_item
